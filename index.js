75345e763e6dddf19c0acc85b0bce5cc81c516d8
RamziChammem <ramzichammem69@gmail.com>
2020-10-19 15:44:58 +0100

    am start getting cunfused and almost stuck :)

--- index.js
+++ index.js
@@ -24,5 +24,52 @@ else if ( email !== "ramzichammem125@gmail.com" || password !== "rebootkamp"){
         },1000)
     }
 }
+function valid(){
+    $("#otherPage1").fadeOut(1000)
+    setTimeout(function(){
+        $("#login").fadeIn(1000)
+    }, 1000)
+}
+function valid1(){
+    $("#otherPage1").fadeOut(1000)
+    setTimeout(function(){
+        $("#otherPageDice").fadeIn(1000)
+    }, 1000)
+}
 $("#stImage").attr("src", "https://vistapointe.net/images/dice-1.jpg")
 $("#ndImage").attr("src", "https://www.auntannie.com/GamesToMake/MatchCards/MatchCardsPlay.gif")
+function playGame(){
+    var randomNumberOne = Math.floor(Math.random()*6)+1;
+    var randomImageOnePath = "./diceImages/"+randomNumberOne+".png"
+
+
+    var randomNumberTwo = Math.floor(Math.random()*6)+1;
+    var randomImageTwoPath = "./diceImages/"+randomNumberTwo+".png"
+
+    var image1 = document.getElementById("dice1");
+    image1.src =  randomImageOnePath;
+    var image2 = document.getElementById("dice2");
+    image2.src =  randomImageTwoPath;
+ 
+    if (randomNumberOne > randomNumberTwo){
+        document.querySelector("#GL").innerHTML="Player One Wins!"
+        
+        
+    }
+    else if (randomNumberOne < randomNumberTwo){
+        document.querySelector("#GL").innerHTML="Player Two Wins!"
+
+    }
+    else {
+        document.querySelector("#GL").innerHTML= "Draw!"
+    }
+}
+document.querySelector(".butn").addEventListener("click", playGame)
+
+function valid2(){
+    $("#otherPageDice").fadeOut(1000)
+    setTimeout(function(){
+        $("#otherPage1").fadeIn(1000)
+    }, 1000)
+    
+}
